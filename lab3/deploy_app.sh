007d88f8532ad6ffd6752e53f1e923f74de21a60
twezowic <01169258@pw.edu.pl>
2023-12-23 13:10:18 +0100

    Try to remove wavefront

--- lab3/deploy_app.sh
+++ lab3/deploy_app.sh
@@ -1,9 +1,32 @@
 git clone https://github.com/spring-petclinic/spring-petclinic-cloud
 
 cd spring-petclinic-cloud/
+sed -i '/- wavefront/d'  manifest.yml
+
+cd k8s/
+
+sed -i '/- name: MANAGEMENT_METRICS_EXPORT_WAVEFRONT_URI/d' api-gateway-deployment.yaml
+sed -i '/value: proxy:\/\/wavefront-proxy.spring-petclinic.svc.cluster.local:2878/d' api-gateway-deployment.yaml
+sed -i 's/replicas: 1/replicas: 2/' api-gateway-deployment.yaml
+
+sed -i '/- name: MANAGEMENT_METRICS_EXPORT_WAVEFRONT_URI/d' customers-service-deployment.yaml
+sed -i '/value: proxy:\/\/wavefront-proxy.spring-petclinic.svc.cluster.local:2878/d' customers-service-deployment.yaml
+sed -i 's/replicas: 1/replicas: 2/' customers-service-deployment.yaml
+
+sed -i '/- name: MANAGEMENT_METRICS_EXPORT_WAVEFRONT_URI/d' vets-service-deployment.yaml
+sed -i '/value: proxy:\/\/wavefront-proxy.spring-petclinic.svc.cluster.local:2878/d' vets-service-deployment.yaml
+sed -i 's/replicas: 1/replicas: 2/' vets-service-deployment.yaml
+
+sed -i '/- name: MANAGEMENT_METRICS_EXPORT_WAVEFRONT_URI/d' visits-service-deployment.yaml
+sed -i '/value: proxy:\/\/wavefront-proxy.spring-petclinic.svc.cluster.local:2878/d' visits-service-deployment.yaml
+sed -i 's/replicas: 1/replicas: 2/' visits-service-deployment.yaml
+
+cd init-services/
+rm 04-wavefront.yaml
+cd ..
+cd ..
 
 kubectl apply -f k8s/init-namespace
-
 kubectl apply -f k8s/init-services
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
